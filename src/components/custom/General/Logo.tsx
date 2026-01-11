6d60c88eb73940a2fd55cfe3c420dbb3df1f1b72
aryanShaw <aryanrot234@gmail.com>
2026-01-12 01:59:11 +0530

    updating the logo

--- src/components/custom/General/Logo.tsx
+++ src/components/custom/General/Logo.tsx
@@ -16,15 +16,19 @@ const Logo = () => {
 
   return (
     <div
-      className="flex items-center pt-1.5 cursor-pointer opacity-[0.85] hover:opacity-100 transition-opacity"
+      className="flex items-center gap-1 px-1 pt-1.5 cursor-pointer opacity-[0.85] hover:opacity-100 transition-opacity"
       onClick={() => router.push("/playground/all")}
     >
       <Image
-        src={mounted && theme === "dark" ? "/logo-dark.svg" : "/logo.svg"}
+        src={
+          mounted && theme === "dark"
+            ? "/icons/logo-dark.svg"
+            : "/icons/logo.svg"
+        }
         alt="Melina Studio"
-        width={35}
-        height={35}
-        className="size-[35px]"
+        width={18}
+        height={18}
+        className="size-[18px] mr-1"
       />
       <span className="text-sm font-medium text-foreground/80 tracking-wide">
         Melina Studio
